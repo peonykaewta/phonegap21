a587d03390ed7c3b5bd3e143930eaa75901f0058
peonykaewta <peonykaewta@gmail.com>
2014-05-31 14:21:46 +0700

    calculater

--- js/app.js
+++ js/app.js
@@ -1,19 +1,14 @@
 //angular.module('myApp', ["mobile-angular-ui"]);
-var app = angular.module('myApp', [
+var app = angular.module('grader', [
   "ngRoute",
   "mobile-angular-ui",
   "mobile-angular-ui.touch",
   "mobile-angular-ui.scrollable"
 ])
-
 app.config(function($routeProvider) {
       $routeProvider.when('/', { 
          templateUrl: 'pages/home.html',
          controller: 'HomeCtrl'
-      }).when('/profile', { 
-         templateUrl: 'pages/profile.html',
-         controller: 'ProfileCtrl'
-      });
+      })
       // ...
   });
-
--- js/controller.js
+++ js/controller.js
@@ -1,11 +1,55 @@
 function HomeCtrl ($scope) {
 	// body...
-	
+	$scope.is_show_grade = false;
+	$scope.calculate = function()
+	{
+		$scope.is_show_grade = true;
+		// if($scope.score == 0)
+		// 	$scope.grade = "F"
+		// else
+		// 	$scope.grade = "P";
+	// 	if(($scope.score >=80)&&($scope.score<=100)) { $scope.grade= "A" ; }
+	// else if (($scope.score>=70)&&($scope.score<=79)) { $scope.grade= "B" ; }
+	// else if (($scope.score>=60)&&($scope.score<=69)) { $scope.grade= "C" ; }
+	// else if (($scope.score>=50)&&($scope.score<=59)) { $scope.grade= "D" ; }
+	// else if (($scope.score>=0)&&($scope.score<=49)) { $scope.grade= "F" ; }
+	// else{imposible}
+	if($scope.score == 0)
+		$scope.grade = "F"
+	if($scope.score > 100 || $scope.score < 0)
+			$scope.grade = "Impossible"
+		else if($scope.score >= 80)
+			$scope.grade = "A";
+		else if($scope.score >= 70)
+			$scope.grade = "B";
+		else if($scope.score >= 60)
+			$scope.grade = "C";
+		else if($scope.score >= 50)
+			$scope.grade = "D";
+		else
+			$scope.grade = "P";
+			$scope.grade = "F";
+	}
+	$scope.add = function(){
+		$scope.is_show_value = true;
+		$scope.value= ($scope.first + $scope.second);
+		
+	}
+	$scope.minus = function(){
+		$scope.is_show_value = true;
+		$scope.value= ($scope.first - $scope.second);
+	}
+	$scope.multiply = function(){
+		$scope.is_show_value = true;
+		$scope.value= ($scope.first * $scope.second);
+	}
+	$scope.divide = function(){
+		$scope.is_show_value = true;
+		$scope.value= ($scope.first / $scope.second);
+	}
+
+
+
+
 }
 
-function ProfileCtrl ($scope) {
-	// body...
-
-	
-
-}
