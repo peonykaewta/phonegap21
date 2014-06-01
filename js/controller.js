76145d788b6dd8e5965dbc254b2ede6cfc7a36b1
peonykaewta <peonykaewta@gmail.com>
2014-06-01 09:57:29 +0700

    add markup for edit hg-repeat
    update controller hg-repeat -edit

--- js/controller.js
+++ js/controller.js
@@ -1,75 +1,38 @@
 function HomeCtrl ($scope) {
-	// body...
-	$scope.test = function(a)
-	{
-	/*if(typeof(a) != 'object')
-			alert(a);
-		else
-			alert(a.age);*/
-		if(angular.isObject(a))
-			alert(a.age);
-		else
-			alert(a)
-	}
-	$scope.people = {name: 'PPP', age: 20}
-	$scope.products=[
-		{name: "Coke",price:20},
-		{name: "Pepsi",price:20},
-		{name: "Banana",price:20},
-		{name: "Fanta",price:20},
-	]
+// body...
+$scope.test = function(a)
+{
+/*if(typeof(a) != 'object')
+alert(a);
+else
+alert(a.age);*/
+if(angular.isObject(a))
+alert(a.age);
+else
+alert(a)
+}
+$scope.people = {name: 'PPP', age: 20}
+$scope.products = [
+{name:"Coke", price:20, is_edit: false },
+{name:"Pepsi", price:10, is_edit: false },
+{name:"Banana", price:25, is_edit: false },
+{name:"Fanta", price:30, is_edit: false }
+]
+
+$scope.edit = function(item)
+{
+item.is_edit = true;
+}
+
+$scope.save = function(item)
+{
+item.is_edit = false;
 
 
-	// $scope.is_show_grade = false;
-	// $scope.calculate = function()
-	// {
-	// 	$scope.is_show_grade = true;
-	// 	// if($scope.score == 0)
-	// 	// 	$scope.grade = "F"
-	// 	// else
-	// 	// 	$scope.grade = "P";
-	// // 	if(($scope.score >=80)&&($scope.score<=100)) { $scope.grade= "A" ; }
-	// // else if (($scope.score>=70)&&($scope.score<=79)) { $scope.grade= "B" ; }
-	// // else if (($scope.score>=60)&&($scope.score<=69)) { $scope.grade= "C" ; }
-	// // else if (($scope.score>=50)&&($scope.score<=59)) { $scope.grade= "D" ; }
-	// // else if (($scope.score>=0)&&($scope.score<=49)) { $scope.grade= "F" ; }
-	// // else{imposible}
-	// if($scope.score == 0)
-	// 	$scope.grade = "F"
-	// if($scope.score > 100 || $scope.score < 0)
-	// 		$scope.grade = "Impossible"
-	// 	else if($scope.score >= 80)
-	// 		$scope.grade = "A";
-	// 	else if($scope.score >= 70)
-	// 		$scope.grade = "B";
-	// 	else if($scope.score >= 60)
-	// 		$scope.grade = "C";
-	// 	else if($scope.score >= 50)
-	// 		$scope.grade = "D";
-	// 	else
-	// 		$scope.grade = "P";
-	// 		$scope.grade = "F";
-	// }
-	// $scope.add = function(){
-	// 	$scope.is_show_value = true;
-	// 	$scope.value= ($scope.first + $scope.second);
-		
-	// }
-	// $scope.minus = function(){
-	// 	$scope.is_show_value = true;
-	// 	$scope.value= ($scope.first - $scope.second);
-	// }
-	// $scope.multiply = function(){
-	// 	$scope.is_show_value = true;
-	// 	$scope.value= ($scope.first * $scope.second);
-	// }
-	// $scope.divide = function(){
-	// 	$scope.is_show_value = true;
-	// 	$scope.value= ($scope.first / $scope.second);
-	// }
+}
+
 
 
 
 
 }
-
